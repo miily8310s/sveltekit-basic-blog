c353e45de7f445ee92b2958c3def7bddab85cf93
miily8310s <rillakerika@gmail.com>
2021-07-05 08:39:45 +0900

    Add index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,47 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+	export const load = async({ fetch }) => {
+		const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+		const posts = await res.json()
+		return {
+			props: {
+				posts
+			}
+		}
+	}
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter/index.svelte';
+	import type { Post } from "$lib/types";
+	export let posts: Post[]
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+<h1>Posts</h1>
+
+<div class="posts">
+	{#each posts as post}
+		<div class="post">
+			<h2>{post.title.slice(0, 20)}</h2>
+			<p>{post.body}</p>
+			<p class="link">Read More</p>
 		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+	{/each}
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+	.posts {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 20px;
 	}
-
-	h1 {
-		width: 100%;
+	.post {
+		padding: 10px;
+		border: 1px solid #ddd;
 	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.link {
+		text-align: right;
 	}
 </style>
